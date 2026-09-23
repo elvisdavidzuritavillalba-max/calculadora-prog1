f33973dc420d440a499f43aa762414e7404cedf0
elvisdavidzuritavillalba-max <elvisdavidzuritavillalba@gmail.com>
2026-09-23 17:45:33 -0400

    Add files via upload

---
+++ Primera_tarea.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primera_tarea;
+
+/**
+ *
+ * @author elvis
+ */
+public class Primera_tarea {
+
+    public static void main(String[] args) {
+        int edad = 20;
+        double promedio = 85.5;
+
+        Integer edadWrapper = edad;
+        Double promedioWrapper = promedio;
+        
+        System.out.println("Edad primitiva: " + edad);
+        System.out.println("Promedio primitivo: " + promedio);
+        System.out.println("Edad wrapper: " + edadWrapper);
+        System.out.println("Promedio wrapper: " + promedioWrapper);
+    }
+}
